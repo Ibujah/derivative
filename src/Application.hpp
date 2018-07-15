26e7bd743fa3709ba9a2602d73e87baef4d7253c
ibujah <bastien.durix@enseeiht.fr>
2018-07-15 13:04:13 +0200

    Some tests

--- src/Application.hpp
+++ src/Application.hpp
@@ -1,116 +1,90 @@
 #ifndef _APPLICATION_H_
 #define _APPLICATION_H_
 
-#include <Eigen/Dense>
-#include <memory>
-
-template<typename output, typename input>
-class Application
+class Zero
 {
     public:
-        using Output = output;
-        using Input = input;
-        using Ptr = std::shared_ptr<Application>;
-
-    public:
-        virtual inline Output operator()(const Input &) const = 0;
-};
-
-
-template<unsigned int Outdim, unsigned int Indim>
-class RealApplication : public Application<Eigen::Matrix<double,Outdim,1>,Eigen::Matrix<double,Indim,1> >
-{
-    public:
-        using Output = Eigen::Matrix<double,Outdim,1>;
-        using Input = Eigen::Matrix<double,Indim,1>;
-        using Ptr = std::shared_ptr<RealApplication>;
-        
-    public:
-        virtual inline Output operator()(const Input &) const = 0;
-};
-
-template<unsigned int Outdim, unsigned int Indim>
-class AffineFun : public RealApplication<Outdim,Indim>
-{
-    public:
-        using Output = Eigen::Matrix<double,Outdim,1>;
-        using Input = Eigen::Matrix<double,Indim,1>;
-        using Ptr = std::shared_ptr<AffineFun>;
-        
-    protected:
-        Eigen::Matrix<double,Outdim,Indim> A;
-        Eigen::Matrix<double,Outdim,1> b;
-
-    public:
-        AffineFun(const Eigen::Matrix<double,Outdim,Indim> &A_, Eigen::Matrix<double,Outdim,1> b_) : A(A_), b(b_) {};
-        virtual inline Output operator()(const Input &in) const
+        template<typename ...Args>
+        static inline double eval(Args... args)
         {
-            return A*in+b;
+            return 0;
         };
 };
 
-template<typename Fct, typename... Args>
-class Compositor : public Application<typename Fct::Output, typename Fct::Input>
+class One
 {
     public:
-        using Output = typename Fct::Output;
-        using Input = typename Fct::Input;
-        using Ptr = std::shared_ptr<Compositor>;
-        
-    protected:
-        typename Fct::Ptr m_fct;
-
-    public:
-        Compositor() : m_fct(std::make_shared<Fct>()) {};
-
-        Compositor(const typename Fct::Ptr fct) : m_fct(fct) {};
-
-        Compositor(const Fct &fct) : Compositor(typename Fct::Ptr(std::make_shared<Fct>(fct))) {};
-
-        inline Output operator()(const Input &t) const
+        template<typename ...Args>
+        static inline double eval(Args... args)
         {
-            return (*m_fct)(t);
+            return 1;
         };
-
-        inline const typename Fct::Ptr getFun() const
-        {
-            return m_fct;
-        }
 };
 
-template<typename Fct, typename Fct_, typename... Args>
-class Compositor<Fct,Fct_,Args...> : public Application<typename Fct::Output,typename Compositor<Fct_,Args...>::Input>
+template<unsigned int n>
+class Argument
 {
     public:
-        using Output = typename Fct::Output;
-        using Input = typename Compositor<Fct_,Args...>::Input;
-        using Ptr = std::shared_ptr<Compositor>;
-
-    protected:
-        typename Fct::Ptr m_fct;
-        Compositor<Fct_,Args...> m_next;
-
-    public:
-        Compositor() : m_fct(std::make_shared<Fct>()), m_next() {};
-
-        Compositor(const typename Fct::Ptr &fct, const typename Fct_::Ptr &fct_, const Args&... fcts) :  m_fct(fct), m_next(fct_, fcts...) {};
-
-        Compositor(const Fct &fct, const Fct_ &fct_, const Args&... fcts) :  m_fct(std::make_shared<Fct>(fct)), m_next(fct_, fcts...) {};
-
-        inline Output operator()(const Input &t) const
+        template<typename ...Args>
+        static inline double eval(const double &x, Args... args)
         {
-            return (*m_fct)(  m_next(t) );
+            return Argument<n-1>::eval(args...);
         };
+};
 
-        inline const Compositor<Fct_,Args...>& next() const
+template<>
+class Argument<0>
+{
+    public:
+        template<typename ...Args>
+        static inline double eval(const double &x, Args... args)
         {
-            return m_next;
-        }
+            return x;
+        };
+};
 
-        inline const typename Fct::Ptr getFun() const
+template<typename O1, typename O2>
+class Plus
+{
+    public:
+        template<typename ...Args>
+        static inline double eval(Args... args)
         {
-            return m_fct;
-        }
+            return O1::eval(args...)+O2::eval(args...);
+        };
+};
+
+template<typename F, typename A>
+struct Der {};
+
+template<typename A>
+struct Der<Zero,A>
+{
+    using der = Zero;
+};
+
+template<typename A>
+struct Der<One,A>
+{
+    using der = Zero;
+};
+
+template<unsigned int n>
+struct Der<Argument<n>,Argument<n> >
+{
+    using der = One;
+};
+
+template<unsigned int n, unsigned int nA>
+struct Der<Argument<n>,Argument<nA> >
+{
+    using der = Zero;
+};
+
+template<typename O1, typename O2, typename A>
+struct Der<Plus<O1,O2>, A>
+{
+    using der = Plus<typename Der<O1,A>::der,typename Der<O2,A>::der>;
 };
 
 #endif//_APPLICATION_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include "Application.hpp"
 
+using X = Argument<0>;
+using Y = Argument<1>;
+using Z = Argument<2>;
+using F = Plus<X,Y>;
+using G = Plus<X,X>;
+using H = Plus<X,One>;
+using dFdX = Der<F,X>::der;
+using dGdX = Der<G,X>::der;
+using dHdX = Der<H,X>::der;
+using ddGddX = Der<Der<G,X>::der,X>::der;
+
 int main()
 {
-    Eigen::Vector2d a(1,2);
-    Eigen::Matrix<double,1,1> b(1);
-    AffineFun<1,2> aff(a.transpose(),b);
+    // what I want:
+    // f(x) = x+4
+    // Plus<X,4> f;
+    // g(x) = (4+x)*(2*x)
+    // Mul<Plus<X,4>,Mul<2,X> > g;
+    // h(x) = g'(x)
+    // auto h = Der<X>(g);
+    // u(x,y) = x+y
+    // Plus<X,Y> u;
+    
     std::cout << "Hello world!" << std::endl;
-    std::cout << aff(Eigen::Vector2d(0,2)) << std::endl;
+    
+    std::cout << "X " << X::eval(0,1,2) << std::endl;
+    std::cout << "Y " << Y::eval(0,1,2) << std::endl;
+    std::cout << "F " << F::eval(2,1) << std::endl;
+    std::cout << "G " << G::eval(2) << std::endl;
+    std::cout << "H " << H::eval(2) << std::endl;
+    std::cout << "dFdX " << dFdX::eval(0) << std::endl;
+    std::cout << "dGdX " << dGdX::eval(0) << std::endl;
+    std::cout << "dHdX " << dHdX::eval(0) << std::endl;
+    std::cout << "ddGddX " << ddGddX::eval(0) << std::endl;
+    // std::cout << Y::eval(0) << std::endl; // error
+
     return 0;
 }
