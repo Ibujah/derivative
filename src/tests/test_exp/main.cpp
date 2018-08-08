5f440d6883fd8e1e19669d9179f3cdc0b467dc8a
DURIX Bastien <bastien.durix@enseeiht.fr>
2018-08-08 17:11:31 +0200

    Rectified test_exp

--- src/tests/test_exp/main.cpp
+++ src/tests/test_exp/main.cpp
@@ -10,13 +10,14 @@
 #include <basic_operations/Exp.hpp>
 
 using F1 = Exp<X>;
-using F2 = Exp<Mult<Negative<1>,X,Inverse<Integer<2> > > >;
+using F2 = Exp<Mult<Negative<1>,X,X,Inverse<Integer<2> > > >;
 
 int main()
 {
 	std::cout.precision(2);
     
 	const double x0 = 1.0;
+	std::cout << "x0 = " << x0 << std::endl;
 	
 	std::cout << F1::write() << " " << F1::eval(x0) << std::endl;
 	std::cout << F2::write() << " " << F2::eval(x0) << std::endl;
